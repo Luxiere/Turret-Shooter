c604204fbee17c0993385d68fe913fb972f90a21
Luxiere <49752840+Luxiere@users.noreply.github.com>
2019-08-13 03:04:20 +0700

    Bug fixes
    Bug fixes to side bounce. Brief stop before explosion

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -26,10 +26,11 @@ public class Bullet : MonoBehaviour
         player = FindObjectOfType<Player>();
         empBlast.SetActive(false);
         yield return new WaitForSeconds(projectileDestroyTime - explosionTime);
-        empBlast.SetActive(true);
         moving = false;
+        yield return new WaitForSeconds(explosionTime / 2);
+        empBlast.SetActive(true);
         //explosion here
-        yield return new WaitForSeconds(explosionTime);
+        yield return new WaitForSeconds(explosionTime / 2);
         Destroy(gameObject);
     }
 
@@ -39,17 +40,35 @@ public class Bullet : MonoBehaviour
         {
             rb.velocity = transform.up * projectileSpeed;
         }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (rb.velocity.x > 0 && rb.velocity.y > 0 || rb.velocity.x < 0 && rb.velocity.y < 0)
+        if (collision.gameObject.name == "Border_U")
         {
-            gameObject.transform.eulerAngles += new Vector3(0f, 0f, directionChange);
+            if (rb.velocity.x > 0)
+            {
+                gameObject.transform.eulerAngles -= new Vector3(0f, 0f, directionChange);
+            }
+            else
+            {
+                gameObject.transform.eulerAngles += new Vector3(0f, 0f, directionChange);
+            }
         }
         else
         {
-            gameObject.transform.eulerAngles -= new Vector3(0f, 0f, directionChange);
+            if (rb.velocity.x > 0 && rb.velocity.y > 0 || rb.velocity.x < 0 && rb.velocity.y < 0)
+            {
+                gameObject.transform.eulerAngles += new Vector3(0f, 0f, directionChange);
+            }
+            else
+            {
+                gameObject.transform.eulerAngles -= new Vector3(0f, 0f, directionChange);
+            }
         }
     }
 }
