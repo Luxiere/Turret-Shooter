4bdc4c32e3df04a30d548f0a52e5e58e364d34b2
Luxiere <49752840+Luxiere@users.noreply.github.com>
2019-08-15 01:18:47 +0700

    Main menu button now working
    Buttons hooked up. Sound options added.

---
+++ Assets/Scripts/OptionController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionController : MonoBehaviour
+{
+    [SerializeField] Slider musicSlider;
+    [SerializeField] Slider soundSlider;
+
+    float savedMusicVolume;
+    float savedSoundVolume;
+
+    private void Start()
+    {
+        musicSlider.value = PlayerPrefsController.GetMusicVolume();
+        soundSlider.value = PlayerPrefsController.GetSoundVolume();
+    }
+
+    public void CacheVolume()
+    {
+        savedMusicVolume = PlayerPrefsController.GetMusicVolume();
+        savedSoundVolume = PlayerPrefsController.GetSoundVolume();
+    }
+
+    public void Save()
+    {
+        PlayerPrefsController.SetMusicVolume(musicSlider.value);
+        PlayerPrefsController.SetSoundVolume(soundSlider.value);
+    }
+
+    public void Cancel()
+    {
+        PlayerPrefsController.SetMusicVolume(savedMusicVolume);
+        PlayerPrefsController.SetSoundVolume(savedSoundVolume);
+    }
+}
---
+++ Assets/Scripts/PlayerPrefsController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPrefsController : MonoBehaviour
+{
+    const string MUSIC_VOLUME_KEY = "music volume";
+    const string SOUND_VOLUME_KEY = "sound";
+
+    const float MIN_MUSIC_VOLUME = 0f;
+    const float MAX_MUSIC_VOLUME = 1f;
+    float currentMusicVolume;
+
+    const float MIN_SOUND_VOLUME = 0f;
+    const float MAX_SOUND_VOLUME = 1f;
+    float currentSoundVolume;
+
+    public static void SetMusicVolume(float volume)
+    {
+        if(volume >= MIN_MUSIC_VOLUME && volume <= MAX_MUSIC_VOLUME)
+        {
+            Debug.Log("Music volume is" + volume);
+            PlayerPrefs.SetFloat(MUSIC_VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("Music volume out of bounds");
+        }
+    }
+
+    public static float GetMusicVolume()
+    {
+        return PlayerPrefs.GetFloat(MUSIC_VOLUME_KEY);
+    }
+
+    public static void SetSoundVolume(float volume)
+    {
+        if(volume >= MIN_SOUND_VOLUME && volume <= MAX_SOUND_VOLUME)
+        {
+            Debug.Log("Sound volume is" + volume);
+            PlayerPrefs.SetFloat(SOUND_VOLUME_KEY, volume);
+        }
+        else
+        {
+            Debug.LogError("Sound volume out of bounds");
+        }
+    }
+
+    public static float GetSoundVolume()
+    {
+        return PlayerPrefs.GetFloat(SOUND_VOLUME_KEY);
+    }
+}
