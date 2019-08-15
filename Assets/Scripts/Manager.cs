da5966577cf330421da764ecc579da8f81d1d8cc
rayoelperro <rayoelperro@gmail.com>
2019-08-15 20:12:33 +0200

    Win conditions and game manager

---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Manager : MonoBehaviour
+{
+    [SerializeField] Level[] levels;
+    private int idx = 0;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+    
+    void Update()
+    {
+        //SceneManager.LoadScene(idx);
+    }
+
+    public int TotalPoints()
+    {
+        int total = 0;
+        foreach (Level l in levels)
+            total += l.points;
+        return total;
+    }
+
+    [System.Serializable]
+    public struct Level
+    {
+        public string Name;
+        [HideInInspector] public int points;
+    }
+
+}
